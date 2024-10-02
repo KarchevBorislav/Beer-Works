a4c9375aaf130f1e7cb821ad78f931856d106f4a
KarchevBorislav <bkarchev@web.de>
2024-10-02 21:04:39 +0200

    added put for beer object

--- src/main/java/spring/framework/beerworks/services/BeerServiceImpl.java
+++ src/main/java/spring/framework/beerworks/services/BeerServiceImpl.java
@@ -95,4 +95,16 @@ public class BeerServiceImpl implements BeerService {
 
         return savedBeer;
     }
+
+    @Override
+    public void updateBeerById(UUID beerId, Beer beer) {
+        Beer existing = beerMap.get(beerId);
+      existing.setBeerName(beer.getBeerName());
+      existing.setQuantityOnHand(beer.getQuantityOnHand());
+      existing.setUpc(beer.getUpc());
+      existing.setPrice(beer.getPrice());
+
+      beerMap.put(existing.getId(), existing);
+
+    }
 }
