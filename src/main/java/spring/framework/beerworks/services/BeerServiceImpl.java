3df8c7ad9c2677dbf0f809c934593e9b68ecf28b
KarchevBorislav <bkarchev@web.de>
2024-10-03 13:14:42 +0200

    added beer patch example

--- src/main/java/spring/framework/beerworks/services/BeerServiceImpl.java
+++ src/main/java/spring/framework/beerworks/services/BeerServiceImpl.java
@@ -2,6 +2,7 @@ package spring.framework.beerworks.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import spring.framework.beerworks.model.Beer;
 import spring.framework.beerworks.model.BeerStyle;
 
@@ -111,6 +112,29 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public void deleteById(UUID beerId) {
         beerMap.remove(beerId);
+
+    }
+
+    @Override
+    public void patchBeerById(UUID beerId, Beer beer)   {
+        Beer existing = beerMap.get(beerId);
+
+        if (StringUtils.hasText(beer.getBeerName())){
+            existing.setBeerName(beer.getBeerName());
+        }
+        if (beer.getBeerStyle() != null){
+            existing.setBeerStyle(beer.getBeerStyle());
+        }
+        if (beer.getPrice() != null){
+            existing.setPrice(beer.getPrice());
+        }
+        if (beer.getQuantityOnHand() != null){
+            existing.setQuantityOnHand(beer.getQuantityOnHand());
+
+        }
+        if (StringUtils.hasText(beer.getUpc())){
+            existing.setUpc(beer.getUpc());
+        }
 
     }
 }
