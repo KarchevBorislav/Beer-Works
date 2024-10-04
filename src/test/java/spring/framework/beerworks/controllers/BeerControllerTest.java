1092e1f580c564c1c37448e1ba345db573537898
KarchevBorislav <bkarchev@web.de>
2024-10-04 06:17:17 +0200

    added mockito configuration

--- src/test/java/spring/framework/beerworks/controllers/BeerControllerTest.java
+++ src/test/java/spring/framework/beerworks/controllers/BeerControllerTest.java
@@ -1,22 +1,36 @@
 package spring.framework.beerworks.controllers;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import spring.framework.beerworks.services.BeerService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 import java.util.UUID;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
+//@SpringBootTest
+@WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
     @Autowired
-    BeerController controller;
+    MockMvc mockMvc;
+
+    @MockBean
+    BeerService beerService;
 
     @Test
-    void getBeerById() {
-
-       // System.out.println(controller.getBeerById(UUID.randomUUID()));
+    void getBeerById() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
-}
+    }
