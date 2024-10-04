7e60a433af0b9f63cc95b6f66a9f2aa562352b1c
KarchevBorislav <bkarchev@web.de>
2024-10-04 13:12:44 +0200

    Update CustomerControllerTest.java

--- src/test/java/spring/framework/beerworks/controllers/CustomerControllerTest.java
+++ src/test/java/spring/framework/beerworks/controllers/CustomerControllerTest.java
@@ -1,11 +1,14 @@
 package spring.framework.beerworks.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import spring.framework.beerworks.model.Beer;
 import spring.framework.beerworks.model.Customer;
 import spring.framework.beerworks.services.CustomerService;
 import spring.framework.beerworks.services.CustomerServiceImpl;
@@ -14,6 +17,7 @@ import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
@@ -24,12 +28,21 @@ class CustomerControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @MockBean
     CustomerService customerService;
 
 
-    CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
+    CustomerServiceImpl customerServiceImpl ;
+    @Autowired
+    private ObjectMapper jacksonObjectMapper;
 
+    @BeforeEach
+    void setUp() {
+        customerServiceImpl = new CustomerServiceImpl();
+    }
 
     @Test
     void testGetCustomerList() throws Exception {
@@ -60,6 +73,26 @@ class CustomerControllerTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON ))
                 .andExpect(jsonPath("$.id",is(customerTest.getId().toString())))
                 .andExpect(jsonPath("$.customerName",is(customerTest.getCustomerName())));
+
+    }
+
+    @Test
+    void testCreateCustomer() throws Exception {
+        Customer customer = customerServiceImpl.getCustomerList().get(1);
+
+        customer.setCustomerName(null);
+        customer.setId(null);
+
+        given(customerService.saveNewCustomer(any(Customer.class))).willReturn(customerServiceImpl.getCustomerList().get(0));
+
+
+        mockMvc.perform(post("/api/v1/customer")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content (jacksonObjectMapper.writeValueAsString(customer)))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"));
+
 
     }
 
