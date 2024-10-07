06879b370fcd21a61c5edf949b4a7dfb3f10d16d
KarchevBorislav <bkarchev@web.de>
2024-10-07 12:50:06 +0200

    Update BeerControllerIT.java

--- src/test/java/spring/framework/beerworks/controllers/BeerControllerIT.java
+++ src/test/java/spring/framework/beerworks/controllers/BeerControllerIT.java
@@ -5,10 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import spring.framework.beerworks.Exceptions.NotFoundException;
+import spring.framework.beerworks.entities.Beer;
 import spring.framework.beerworks.model.BeerDTO;
 import spring.framework.beerworks.repositories.BeerRepository;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -37,5 +40,20 @@ class BeerControllerIT {
         List<BeerDTO> beerDTOS = beerController.beerList();
 
         assertThat(beerDTOS.size()).isEqualTo(0);
+    }
+
+    @Test
+    void testGetBeerById(){
+        Beer beer = beerRepository.findAll().get(0);
+
+        BeerDTO beerByIdDTO = beerController.getBeerById(beer.getId());
+
+        assertThat(beerByIdDTO).isNotNull();
+    }
+
+    @Test
+    void testBeerIdNotFound(){
+        assertThrows(NotFoundException.class, () -> beerController.getBeerById(UUID.randomUUID()));
+
     }
 }
