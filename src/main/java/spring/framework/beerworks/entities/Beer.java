de905d0793ad47d9542f45efe905119630c2255d
KarchevBorislav <bkarchev@web.de>
2024-10-12 11:28:36 +0200

    Update Beer.java

--- src/main/java/spring/framework/beerworks/entities/Beer.java
+++ src/main/java/spring/framework/beerworks/entities/Beer.java
@@ -5,8 +5,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.type.SqlTypes;
 import spring.framework.beerworks.model.BeerStyle;
 
@@ -50,6 +52,11 @@ public class Beer {
     private Integer quantityOnHand;
     @NotNull
     private BigDecimal price;
+
+    @CreationTimestamp
     private LocalDateTime createDate;
+
+
+    @UpdateTimestamp
     private LocalDateTime updateDate;
 }
